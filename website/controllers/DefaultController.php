9e3b1dc57ae4966872cd88f17e91b425e4e81746
vikash kumar <vikash.kumar@osscube.com>
2016-02-01 15:28:31 +0530

    strucrure of nyt added

--- website/controllers/DefaultController.php
+++ website/controllers/DefaultController.php
@@ -5,6 +5,14 @@ use Website\Controller\Action;
 class DefaultController extends Action {
 	
 	public function defaultAction () {
-
+		$this->enableLayout();
+             
+	}
+    public function headerAction () {
+		
+	}
+	
+	public function footerAction () {
+	
 	}
 }
---
+++ website/views/layouts/layout.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>The New York Times: Get a Digital Subscription</title>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="sourceApp" content="MOPS"/>
+    <meta name="WT.cg_n" content="Digital Subscription"/>
+    <meta name="WT.z_gpt" content="E-Commerce"/>
+    <meta name="WT.z_gpst" content="Purchase"/>
+    <meta name="WT.si_n" content="Digital Subscription"/>
+    <meta name="WT.si_x" content="2"/>
+    <meta name="WT.ti" content="Landing Page - NYT"/>
+    <meta name="WT.cg_s" content="NYT"/>
+    <meta name="DCS.dcssip" content="www.nytimes.com"/>
+    <meta name="DCS.dcsuri" content="/subscriptions/Multiproduct/lp5558.html"/>
+    <meta name="WT.z_acqprod" content="A, B, D"/>
+    <meta property="og:title" content="I just became a New York Times digital subscriber"/>
+    <meta property="og:site_name" content="nytimes.com/subscribenyt"/>
+    <meta property="og:image" content="http://www.nytimes.com/subscriptions/Multiproduct/img/laptop_50x50.png"/>
+    <meta property="og:type" content="product"/>
+    <meta property="og:url" content="http://www.nytimes.com/subscriptions/Multiproduct/lp5558.html"/>
+    <meta property="og:description"
+          content="Join me in supporting award-winning journalism and enjoy unlimited access to NYTimes.com 24 hours a day."/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="canonical" href="http://www.nytimes.com/subscriptions/Multiproduct/lp5558.html"/>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="website/static/js/html5shiv.js"></script>
+    <script src="website/static/js/common.js"></script>
+
+    <!-- NYTimes typekit -->
+    <script src="//typeface.nytimes.com/zam5nzz.js"></script>
+    <script>try {
+        Typekit.load();
+    } catch (e) {
+    }</script>
+
+
+    <link rel="stylesheet" type="text/css" media="all" href="website/static/css/digi-core-grid_refactor.css"/>
+</head>
+
+<body>
+<?= $this->inc("/home/header");?>
+
+<?= $this->layout()->content ?>
+
+<?= $this->inc("/home/footer"); ?>
+</body>
+</html>
+
+	
