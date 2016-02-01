10c2a206745762ec5837b0779aac59c1efdaa27a
vikash kumar <vikash.kumar@osscube.com>
2016-02-01 17:30:38 +0530

    editable added for header

--- website/views/scripts/default/header.php
+++ website/views/scripts/default/header.php
@@ -15,15 +15,21 @@
             </ul>
         </div>
 
-
+<div class="mainHeader">
         <div class="title desktop">
 			<span>
 			<?= $this->textarea("titleDesc") ?></span>
         <br>
         <span id="sub_title"><?= $this->input("titlePrice"); ?></span></div>
 
-        <div class="title moflow">Try the Times Digital Experience<br><span id="orange">99¢ for 4 weeks</span>
+        <div class="title moflow">Try the Times Digital Experience<br>
+        <span id="orange">99¢ for 4 weeks</span>
         </div>
+        <div id="titleLogo"><?= $this->image("title-logo", array(
+    "title" => "The New York Times",
+
+   
+)); ?></div></div>
 </section>
 
 
