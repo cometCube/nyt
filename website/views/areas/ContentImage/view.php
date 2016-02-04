dbe0f27682ed6bc499faa25a4ee94fb93e0255ee
rohitosscube <rohit.dhiman@osscube.com>
2016-02-04 11:48:58 +0530

    Change Image

--- website/views/areas/ContentImage/view.php
+++ website/views/areas/ContentImage/view.php
@@ -1,3 +1,3 @@
- <div style="width:100%; height:100px; margin-bottom:10px; display:block; clear:both;"><?= $this->image("main_image", ["height" => 100]) ?>
+ <div style="width:100%; height:100px; margin-bottom:10px; display:block; clear:both;"><?= $this->image("main_image") ?>
 </div>
 
